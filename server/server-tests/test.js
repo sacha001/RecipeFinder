9eadd7eae11078c250939f8fec54ac9267a7607e
sacha001 <ssilance@unb.ca>
2020-11-22 00:30:03 -0400

    Updated test and travis file

--- server/server-tests/test.js
+++ server/server-tests/test.js
@@ -1,7 +1,7 @@
 // Import the dependencies for testing
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../server.js');// Configure chai
+const app = require('../src/server.js');// Configure chai
 
 chai.use(chaiHttp);
 chai.should();describe("Students", () => {
