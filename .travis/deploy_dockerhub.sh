26dee1996f61a8299f014b6ac2a44633dd2289f7
Sacha Silance <ssilance@unb.ca>
2020-11-25 00:09:58 -0400

    update sh script

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 docker login -u $DOCKER_USER -p $DOCKER_PASS
+<<<<<<< Updated upstream
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
@@ -7,3 +8,7 @@ else
 fi
 docker build -t $TRAVIS_REPO_SLUG:$TAG ./client
 docker push $TRAVIS_REPO_SLUG:$TAG
+=======
+docker build -t "recipefinder:latest" ./client
+docker push "recipefinder:latest"
+>>>>>>> Stashed changes
